83a6b1dad22f40a5565676213f65189b5fb72b78
MairwunNx <mairwunnx@gmail.com>
2026-08-15 00:37:46 +0300

    Default event parameters to non-null

---
+++ api/src/main/java/dev/ancaria/coderpack/api/event/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * The events a mod subscribes to.
+ *
+ * <p>Parameters here are non-null unless a parameter says otherwise. A getter
+ * that reads a wire field the agent may not have sent is {@code @Nullable}.
+ */
+@ParametersAreNonnullByDefault
+package dev.ancaria.coderpack.api.event;
+
+import javax.annotation.ParametersAreNonnullByDefault;
