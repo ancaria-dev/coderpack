2f0e06b7538dfd3206963c08818705180eeb5545
MairwunNx <mairwunnx@gmail.com>
2026-08-14 21:35:21 +0300

    Default entity parameters to non-null

---
+++ api/src/main/java/dev/ancaria/coderpack/api/entity/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Handles on things in the world: the hero, an item, the class it belongs to.
+ *
+ * <p>Parameters here are non-null unless a parameter says otherwise.
+ */
+@ParametersAreNonnullByDefault
+package dev.ancaria.coderpack.api.entity;
+
+import javax.annotation.ParametersAreNonnullByDefault;
