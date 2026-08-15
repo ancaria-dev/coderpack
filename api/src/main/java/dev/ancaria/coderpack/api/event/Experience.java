39df58978282818a6c2ad8ef863900eb88f02ea3
MairwunNx <mairwunnx@gmail.com>
2026-08-16 01:15:15 +0300

    Add experience change event

---
+++ api/src/main/java/dev/ancaria/coderpack/api/event/Experience.java
@@ -0,0 +1,31 @@
+package dev.ancaria.coderpack.api.event;
+
+import java.util.Map;
+
+/**
+ * Experience is about to be granted. Vetoable via the new total, which the game
+ * clamps to 2,586,931,599 on its own -- Coderpack additionally caps at int32 so a
+ * large multiplier cannot overflow on the way there.
+ */
+public final class Experience extends Veto {
+
+    public Experience(Map<String, String> fields) {
+        super(fields);
+    }
+
+    public long gain() {
+        return num("gain");
+    }
+
+    public long exp() {
+        return num("prev");
+    }
+
+    public long next() {
+        return num("next");
+    }
+
+    public void next(long value) {
+        rewrite("next", value);
+    }
+}
